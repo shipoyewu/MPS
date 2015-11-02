08f0484bdc96d4238e361ff0039e1ac3002d93c6
xingjiali <xingjiali@carrie>
2015-11-02 20:34:52 +0800

    first

---
+++ src/com/zzu/daoImp/ContentDaoImp.java
@@ -0,0 +1,19 @@
+package com.zzu.daoImp;
+
+import com.zzu.dao.ContentDao;
+import com.zzu.modle.Content;
+
+public class ContentDaoImp implements ContentDao {
+
+	@Override
+	public Content getContent(long contentid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
